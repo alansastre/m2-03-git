0bdf01958b02828275d97b8b69992d465ee9a5cb
Ivan <ivanmartinblanco1985@gmail.com>
2021-07-13 13:32:05 +0200

    Commit2 Iván Martín

--- src/com/example/IvanMartin.java
+++ src/com/example/IvanMartin.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class IvanMartin {
  public static void main(String[] args) {
-	System.out.println("Vamos que aprobamos!");
+	System.out.println("Vamos que aprobamos!!");
 }
 }
