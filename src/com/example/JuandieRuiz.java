b71ddbcba17bcb9cf0bc16ffcf9339966fd1f11a
juandiegoruiz <juandieruiz6@gmail.com>
2021-07-13 21:42:49 +0200

    # Repaso de Subida Archivos Git

--- src/com/example/JuandieRuiz.java
+++ src/com/example/JuandieRuiz.java
@@ -5,7 +5,10 @@ public class JuandieRuiz {
 		
 		System.out.println("alan commit");
 		System.out.println("Creando conflicto");
-
+		byte numero = 10;
+		numero = 10 * 10;
+		
+		System.out.println("el valor es: " + numero);
 		
 	}
 }
