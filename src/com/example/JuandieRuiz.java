d89aeab8d6e07daa1bc1035ce707b028dd6a4d1c
Ivan <ivanmartinblanco1985@gmail.com>
2021-07-13 14:31:09 +0200

    Merge branch 'master' of https://github.com/alansastre/m2-03-git.git

--- src/com/example/JuandieRuiz.java
+++ src/com/example/JuandieRuiz.java
@@ -4,7 +4,7 @@ public class JuandieRuiz {
 	public static void main(String[] args) {
 		
 		System.out.println("alan commit");
-		System.out.println("Creando conflicto");
+		System.out.println("Creando conflictoasdfasdf");
 
 		
 	}
