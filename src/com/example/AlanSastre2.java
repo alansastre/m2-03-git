2409129c1b42ef060c4aef218c4ab56bb5462040
alansastre <alan@alansastre.co>
2021-07-13 14:14:00 +0200

    desarrollo1 cambio1

--- src/com/example/AlanSastre2.java
+++ src/com/example/AlanSastre2.java
@@ -4,5 +4,11 @@ public class AlanSastre2 {
 
 	public static void main(String[] args) {
 		System.out.println("cambios version 2");
+		
+		// rama desarrollo1
+		
+		
+		System.out.println("cambio1");
+		System.out.println("cambio2");
 	}
 }
