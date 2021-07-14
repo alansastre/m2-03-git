8f4d81935ca994a1b9db8a1f973be955d450bb0c
Ricardo <rnogueradrum@gmail.com>
2021-07-14 13:17:48 +0200

    Merge branch 'master' of https://github.com/alansastre/m2-03-git
    Conflicts:
    	src/com/example/AlanSastre.java
    	src/com/example/JuandieRuiz.java

--- src/com/example/AlanSastre.java
+++ src/com/example/AlanSastre.java
@@ -6,6 +6,7 @@ public class AlanSastre {
 
 
 		System.out.println("alan sastre");
+<<<<<<< master
 
 		System.out.println("Cambios desde rama ManuelMM");
 
@@ -15,5 +16,17 @@ public class AlanSastre {
 		
 		System.out.println("Commit Juan Diego, prueba");
 
+=======
+<<<<<<< HEAD
+		System.out.println("Cambios desde rama ManuelMM");
+
+		System.out.println("alan sastre prueba desde rama alansastre");
+		
+		System.out.println("fin ddel desarrollo");
+=======
+		
+		System.out.println("Commit Juan Diego, prueba");
+>>>>>>> refs/heads/JuanDiegoGomezRuiz
+>>>>>>> 11c205d Merge branch 'master' of https://github.com/alansastre/m2-03-git
 	}
 }
