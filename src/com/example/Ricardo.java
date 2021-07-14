f3bcd2af5ce35546a78dfe692717e5c406333ee2
Ricardo <rnogueradrum@gmail.com>
2021-07-14 13:16:33 +0200

    commit merge Ricardo

--- src/com/example/Ricardo.java
+++ src/com/example/Ricardo.java
@@ -5,8 +5,8 @@ public class Ricardo {
 	public static void main(String[] args) {
 		System.out.println("Hola, soy Ricardo");
 		
-		
 		System.out.println("Cambio en la rama RicardoNoguera");
+		System.out.println("Merge rama RicardoNoguera");
 
 	}
 
