14311dcd1a63c9b20603d37fd383e655ae0a2598
sfuen <sfuen@DESKTOP-SOQ81IE>
2021-07-14 13:03:50 +0200

    Commit merge Sergio

--- src/com/example/SergioFuentes.java
+++ src/com/example/SergioFuentes.java
@@ -4,7 +4,5 @@ public class SergioFuentes {
 public static void main(String[] args) {
 	System.out.println("Sergio");
 	
-	System.out.println("Cambio hecho antes del descanso");
-	System.out.println("Cambio hecho antes del descanso");
 }
 }
