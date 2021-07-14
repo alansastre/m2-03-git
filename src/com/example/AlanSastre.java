8610dd96c65d4ff66bfa8c35e6ee7e0176389dbc
alansastre <alan@alansastre.co>
2021-07-14 11:43:01 +0200

    commit desde rama filtros

--- src/com/example/AlanSastre.java
+++ src/com/example/AlanSastre.java
@@ -4,5 +4,11 @@ public class AlanSastre {
 
 	public static void main(String[] args) {
 		System.out.println("alan sastre");
+		
+		// filtro por color
+		
+		// ... 
+		
+		System.out.println("Filtro por color");
 	}
 }
