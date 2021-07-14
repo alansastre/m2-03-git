703359e651aa83a5de3e82d8bd097dfff212a69f
sfuen <sfuen@DESKTOP-SOQ81IE>
2021-07-14 12:25:58 +0200

    Commit Sergio Prueba

--- src/com/example/SergioFuentes.java
+++ src/com/example/SergioFuentes.java
@@ -3,5 +3,8 @@ package com.example;
 public class SergioFuentes {
 public static void main(String[] args) {
 	System.out.println("Sergio");
+	
+	System.out.println("Cambio hecho antes del descanso");
+	System.out.println("Cambio hecho antes del descanso");
 }
 }
