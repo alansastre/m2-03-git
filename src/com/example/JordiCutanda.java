eb9539d1fe62bc908d63039669f106eb5806540d
KuXarito <kuxarito@jordis-mac-mini.home>
2021-07-14 13:14:46 +0200

    battle Merge!!

--- src/com/example/JordiCutanda.java
+++ src/com/example/JordiCutanda.java
@@ -3,7 +3,7 @@ package com.example;
 public class JordiCutanda {
 
 	public static void main(String[] args) {
-		System.out.println("Cambio en mi clase para subir branch");
+		System.out.println("Cambio en mi clase para subir branch 2");
 	}
 
 }
