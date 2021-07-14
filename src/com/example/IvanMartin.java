67ed6d294fbfcb12cd6cb6fd8dce6578cef8a049
Ivan <ivanmartinblanco1985@gmail.com>
2021-07-14 12:01:52 +0200

    Commit del descanso para mergear después en mi rama

--- src/com/example/IvanMartin.java
+++ src/com/example/IvanMartin.java
@@ -4,5 +4,7 @@ public class IvanMartin {
  public static void main(String[] args) {
 	System.out.println("Vamos que aprobamos!!");
 	System.out.println("Commit desde la rama Iván Martín Blanco");
+	
+	System.out.println("Este es un cambio para mergear después del descanso");
 }
 }
