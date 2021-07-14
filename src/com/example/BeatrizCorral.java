029c718548675db295e1453c28fc2e48c092d459
Bcorral <beatrizcorralcampos@gmail.com>
2021-07-14 13:05:42 +0200

    commit cambiando a master2

--- src/com/example/BeatrizCorral.java
+++ src/com/example/BeatrizCorral.java
@@ -4,6 +4,7 @@ public class BeatrizCorral {
 	public static void main(String[] args) {
 		System.out.println("Beatriz");
 		System.out.println("cambio desdes mi rama");
+		System.out.println("cambio a master");
 	
 }
 }
