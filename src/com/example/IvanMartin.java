884bbb6994d01bd1cdceaaf350cdbd1a6abafc39
Ivan <ivanmartinblanco1985@gmail.com>
2021-07-14 11:51:41 +0200

    Merge branch 'master' of https://github.com/alansastre/m2-03-git.git

--- src/com/example/IvanMartin.java
+++ src/com/example/IvanMartin.java
@@ -3,5 +3,6 @@ package com.example;
 public class IvanMartin {
  public static void main(String[] args) {
 	System.out.println("Vamos que aprobamos!!");
+	System.out.println("Commit desde la rama Iván Martín Blanco");
 }
 }
