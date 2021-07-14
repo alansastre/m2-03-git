876521d0fec7806a112a0af171e9f05ec72b544e
alansastre <alan@alansastre.co>
2021-07-14 12:39:14 +0200

    commit rama alansastre

--- src/com/example/AlanSastre.java
+++ src/com/example/AlanSastre.java
@@ -4,5 +4,7 @@ public class AlanSastre {
 
 	public static void main(String[] args) {
 		System.out.println("alan sastre prueba desde rama alansastre");
+		
+		System.out.println("fin ddel desarrollo");
 	}
 }
