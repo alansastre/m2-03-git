06ce299c321dcf262e4eaf59a052cd72a2b55a20
Ricardo <rnogueradrum@gmail.com>
2021-07-14 12:26:57 +0200

    commit cambio en rama RicardoNoguera

--- src/com/example/Ricardo.java
+++ src/com/example/Ricardo.java
@@ -3,7 +3,10 @@ package com.example;
 public class Ricardo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola, soy Ricardo ");
+		System.out.println("Hola, soy Ricardo");
+		
+		
+		System.out.println("Cambio en la rama RicardoNoguera");
 
 	}
 
