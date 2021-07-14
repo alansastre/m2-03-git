2059b3c1dd219ac444178573e12bdb629cf95d87
KuXarito <kuxarito@jordis-mac-mini.home>
2021-07-14 13:25:04 +0200

    Cambios de ajuste

--- src/com/example/BeatrizCorral.java
+++ src/com/example/BeatrizCorral.java
@@ -5,8 +5,5 @@ public class BeatrizCorral {
 	public static void main(String[] args) {
 		System.out.println("My mensaje cambiado");
 	}
-		System.out.println("Beatriz");
-		System.out.println("cambio desdes mi rama");
-		System.out.println("cambio a master");
 }
 
--- src/com/example/JuandieRuiz.java
+++ src/com/example/JuandieRuiz.java
@@ -3,8 +3,6 @@ package com.example;
 public class JuandieRuiz {
 
 	public static void main(String[] args) {
-		System.out.println("My mensaje");
-	public static void main(String[] args) {
 
 		System.out.println("alan commit");
 
